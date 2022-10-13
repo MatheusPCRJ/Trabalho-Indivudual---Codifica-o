3f6e6ba45a0df322efdce9bfed72b47abbe3407e
Matheus Gomes <112782424+MatheusPCRJ@users.noreply.github.com>
2022-10-12 23:36:25 -0300

    Design e Js feito.
    Modificações;
    Design quase 95% / 100%.
    Faltando alguns ajustes pessoais.
    JavaScript finalizado.
    Faltou uma opção para concluir 100% dos Extras; Ignorar espaços e caracteres especiais*

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
-# Cifra de Cesar e Base64
-2° Trabalho Individual - Resilia <br>
-Funções: Codificar e Decodificar em Cifra de Cesar e Base64.
+# 2° Projeto Individual - Cidificações de Mensagens 🔐
+
+<div id="inicio" align=center>
+  <a href="#sobre">Sobre</a>&nbsp;&nbsp;&nbsp;
+  <a href="#sobre">Extras</a>&nbsp;&nbsp;&nbsp;
+  <a href="#andamento">Andamento</a>&nbsp;&nbsp;&nbsp;
+  <a href="#linguagens">Softwares & Ferramentas</a>&nbsp;&nbsp;&nbsp;
+  <a href="#autor">Autor</a> 
+</div>
+
+<h2 id="sobre">Sobre 🔎</h2>
+  <p>Projeto onde precisei desenvolver uma aplicação capaz de codificar e decodificar mensagens utilizando Cifra de César e Base64. Utilizando meus conhecimentos de HTML, CSS e JavaScript, com foco em manipulação de DOM, que contenha os requisitos abaixo:</p>
+
+  - Um campo de entrada textual, da mensagem que será codificada ou decodificada
+  - Um campo de seleção, com as opções "cifra de césar" e "base64" com o seguinte comportamento: <br>
+    Com "cifra de césar" ativo na seleção, um novo campo deve surgir no formulário para que seja possível fornecer o incremento utilizado na cifra
+  - Dois campos radiais (radio buttons) com os textos: "codificar" e "decodificar"
+  - Um botão que, com "codificar" selecionado exibe o texto "Codificar Mensagem!" e com "decodificar" selecionado exibe "Decodificar Mensagem!"
+  - Um outro campo textual deve ser utilizado na página para que o resultado da codificação 
+  e decodificação possa ser exibido para a pessoa usuária.
+
+<h2 id="extras">Requisitos Extras 🔎</h2>
+
+- Na Cifra de César ser capaz de diferenciar maiúsculas e minúsculas,
+ou seja, se a letra for maiúscula, a resposta deve conter uma letra
+maiúscula incrementada e se for minúscula, uma minúscula. Ex: “Oi”
+com 2 de incremento vira “Qk”.
+<br>
+- Ter um texto descritivo contextualizando a pessoa usuária sobre o
+projeto apresentado e ensinando a usá-lo.
+<br>
+
+
+
+<h2 id="andamento">Andamento do projeto 📈</h2>
+
+  ![Badge](https://img.shields.io/website?down_message=offline&label=status&style=for-the-badge&up_color=g&up_message=concluido&url=https%3A%2F%2Fytallobruno.github.io%2FProjetoMensagemEscondida%2F)
+
+<h2 id="linguagens">Softwares & Ferramentas utilizadas nesse projeto 📚</h2>
+
+  - [x] HTML5
+  - [x] CSS3
+  - [x] JavaScript
+  - [x] Git
+
+
+
+<div id="autor" align="center">
+  
+  **Criado e desenvolvido por [Matheus Gomes](https://www.linkedin.com/in/matheus-gomes-780339211/).**
+  
+ <div align="center"> 
+  <a href="mailto:ytallodev@gmail.com"><img src="https://cdn-icons-png.flaticon.com/512/552/552486.png" height="40em" title="Enviar E-mail"></a>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href="https://github.com/MatheusPCRJ" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/733/733553.png" height="40em" title="GitHub de MatheusPCRJ"></a>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  <a href="https://www.linkedin.com/in/matheus-gomes-780339211/" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/145/145807.png" height="40em" title="LinkedIn de Matheus Gomes"></a>
+  </div>
+</div>
+
+<br>
+
+<div align="center">
+  &#11165;&nbsp;<a href="#inicio"><strong>Voltar ao topo</strong></a>&nbsp;&#11165;
+</div>
--- js/cifra.js
+++ js/cifra.js
@@ -1,44 +1,120 @@
-/*let resultado = somar(15, 20);
-console.log(resultado);
 
-function somar (number1, number2){
-    return number1 + number2;
+/* javascrip para escolher Cifra de Cesar e aparecer o campo */
+let x = document.querySelector("#escolherCod");
+
+x.addEventListener("change", function(evento){
+    evento.preventDefault();
+
+    let adicionar = document.querySelector("#addlista");
+    
+    if(evento.target.value == "radio-cesar"){
+        adicionar.style = "display: flex";
+    } else {
+        adicionar.style = "display: none";
+    }
+});
+
+
+
+/* javascrip para mudar o nome do botao quando selecionado */
+let botao = document.querySelectorAll('input[name="codification"]');
+
+
+botao.forEach(function(radio){
+    radio.addEventListener("change", function(evento){
+        evento.preventDefault();
+
+        let button = document.querySelector("#codificar-btn");
+
+        if(evento.target.value == "Decodificar"){
+            button.innerHTML = "Decodificar Mensagem";
+        }
+        else{
+            button.innerHTML = "Codificar Mensagem";
+        }
+
+    })  
+});
+
+
+
+let formulario = document.forms.formularioName;
+
+formulario.addEventListener("submit", function (evento) {
+    evento.preventDefault();
+
+    let writing = formulario.textoEntrada.value;
+
+    let choiceCifraBase = formulario.chooseCod.value;
+
+    let radiosCodeDecod = formulario.codification.value;
+
+    let houses = formulario.numberCesar.value;
+
+    let finalMessage = "";
+
+    if (choiceCifraBase == "radio-cesar"){
+        finalMessage = cesar (writing, houses, radiosCodeDecod);
+    } else {
+        finalMessage = base64 (writing, radiosCodeDecod);
+    }
+
+    let finalResul = document.querySelector("#text-codi-saida");
+    finalResul.innerHTML= `${finalMessage}`
+});
+
+
+/*função do Cifra de Cesar */
+function cesar (writing, houses, radiosCodeDecod){
+    houses = Number(houses);
+
+    let finalMessage = "";
+
+    for (let i = 0; i < writing.length; i++){
+        let letra = writing[i];
+        let codigo = letra.charCodeAt();
+
+        if (radiosCodeDecod == "codificar"){
+            codigo += houses;
+        }
+        else{
+            codigo -= houses;
+        }
+        finalMessage += String.fromCharCode(codigo);
+    }
+    return finalMessage;
+}
+/* Final da Função Cifra de Cesar */
+
+
+
+/* Base64 do w3School */
+function base64 (writing, radiosCodeDecod) {
+    if (radiosCodeDecod == "codificar") {
+        return btoa(writing);
+    } else {
+        return atob(writing);
+    }
 }
 
-let inserir = document.querySelector("h2");
-inserir.innerText = resultado;
+
+
+
+
+
+/* variavel que pega o ID do texto 
+let pegaMesagem = document.querySelector("#text-codi");*/
+
+
+
+/* variavel que pega as casas adicionais do Cifra de Cesar
+let CasaCesar = document.querySelector("number-cesar"); *//* div number-cesar que pega os numeros
+let casas = CasaCesar.value; 
+let alfabeto = "abcdefghijklmnopqrstuvwxyz";
+let PegarLetra = alfabeto[casas];   
+console.log(casas);
 */
-
-
-
-
 /*
-let soma = function(one, two){
-    return one + two;
-}
-
-let resul = soma(123, 432);
-console.log(resul);
-
-let x = document.querySelector('h2');
-x.innerText = "Bem-vindo " + resul;
-*/
-
-
-
-/*
-let somar = (um, dois) => {
-    return um + dois;
-}
-
-let x = somar(555, 423);
-let a = document.querySelector('h2');
-a.innerText = "Bem-Vindo " + x;
-
-*/
-
-let agenda = {
-    nome: 'Oliveira',
-    sobrenome: 'Santos',
-}
-console.log(agenda);
+var input = document.querySelector("#number-cesar");
+var texto = input.value;
+console.log(texto);*/
